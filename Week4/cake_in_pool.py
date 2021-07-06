d4390ee9f5008b1099ad1295ea6a41662fd377f0
Thanaporn  Cheenthada <65031520+eye54067@users.noreply.github.com>
2021-07-06 15:25:58 +0700

    Add files via upload

---
+++ Week4/cake_in_pool.py
@@ -0,0 +1,25 @@
+import asyncio
+import pandas as pd
+from bscscan import BscScan
+
+YOUR_API_KEY = "FQN6F7F2VNUZD593B1GP9AMQGR77HWC39W"
+
+async def main():
+  async with BscScan(YOUR_API_KEY) as bsc:
+    cake_in_pool = await bsc.get_bep20_token_transfer_events_by_address_and_contract_paginated(
+            contract_address = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
+            address= "0x0eD7e52944161450477ee417DE9Cd3a859b14fD0",
+            page= 1,
+            offset= 1000,
+            sort= "asc"
+    )
+
+    df = pd.DataFrame(cake_in_pool)
+    print(df)
+    df.to_csv(r'D:\Year3-2\Vulcan\Week4\cake_in_pool.csv')
+    
+if __name__ == "__main__":
+  asyncio.run(main())
+
+#0x0eD7e52944161450477ee417DE9Cd3a859b14fD0
+#0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82
